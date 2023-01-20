973860ade10fd9d2d2616bd33cecf12a9733b803
Alansushi <age_allison@hotmail.com>
2023-01-19 23:51:54 -0600

    Add spline first model

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,14 +1,34 @@
+import Spline from "@splinetool/react-spline";
+import "./projects.css";
+
 export default function Projects() {
   return (
-    <div>
-      <div className="container">
-        <div>Hi projects</div>
-        <div className="row">
-          <div className="col col-4">Proyecto 1</div>
-          <div className="col col-4">Proyecto 2</div>
-          <div className="col col-4">Proyecto 3</div>
+    <div id="father">
+      <div className="row">
+        <h4 className="titles">Diseñamos tus espacios</h4>
+        <div id="spline">
+          <Spline scene="https://prod.spline.design/z4tvbUbQ88wsVNDQ/scene.splinecode" />
         </div>
       </div>
     </div>
   );
 }
+
+// export default function Projects() {
+//   return (
+//     <div>
+//       <div id="projectsBody" className="container">
+//         <div>Hi projects</div>
+//         <div className="row">
+//           <Spline
+//             id="proy1"
+//             scene="https://prod.spline.design/z4tvbUbQ88wsVNDQ/scene.splinecode"
+//           />
+//           <div className="col col-4"></div>
+//           <div className="col col-4">Proyecto 2</div>
+//           <div className="col col-4">Proyecto 3</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
